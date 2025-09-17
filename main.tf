de642599b358f0287744d994a3884551fee7cf1a
AmNat789 <amit.natheniel@gmail.com>
2025-09-17 19:42:51 +0200

    Add AWS ECR repository and outputs to Terraform configuration

--- main.tf
+++ main.tf
@@ -11,3 +11,24 @@ terraform {
 provider "aws" {
   region = "eu-central-1"
 }
+
+# ECR Repository
+resource "aws_ecr_repository" "sttf_api" {
+  name = "sttf-api"
+}
+
+# Outputs
+output "ecr_repository_url" {
+  description = "The URL of the ECR repository"
+  value       = aws_ecr_repository.sttf_api.repository_url
+}
+
+output "ecr_repository_arn" {
+  description = "The ARN of the ECR repository"
+  value       = aws_ecr_repository.sttf_api.arn
+}
+
+output "ecr_registry_id" {
+  description = "The registry ID where the repository was created"
+  value       = aws_ecr_repository.sttf_api.registry_id
+}
